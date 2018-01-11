9b5755426932b436d6f12f6a25713ad57e12f7ac
DESKTOP-V0ENI3K\LG <jieun3001@gmail.com>
2018-01-11 20:39:47 +0900

    회원가입 email인증

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
-
+const mysql = require('mysql');
+const pool = require('../config/dbPool');
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt-nodejs');
+const async = require('async');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.send('respond with a resource');
 });
+
 
 module.exports = router;
