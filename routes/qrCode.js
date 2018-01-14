80360054d3fd23b561245677de030554f4b6025f
DESKTOP-V0ENI3K\LG <jieun3001@gmail.com>
2018-01-14 19:42:41 +0900

    이미지 업로드 test

--- routes/qrCode.js
+++ routes/qrCode.js
@@ -1,5 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const mysql = require('mysql');
+const pool = require('../config/dbPool');
+const express = require('express');
+const router = express.Router();
+const async = require('async');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+aws.config.loadFromPath = ('../config/aws_config.json');
+const s3 = new aws.S3();
+const upload = multer({
+    storage : multerS3({
+        s3: s3,
+        bucket : 'parcel-box',
+        acl: 'public-read',
+        key : function (req, file, cb){
+            cb(null, Date.now() +'.'+ file.originalname.split('.').pop())
+        }
+    })
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
