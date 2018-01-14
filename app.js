f19ea002ba9e3264e9b55ef7409d4e74f01cda21
고민주 <ggmmjj1@gmail.com>
2018-01-14 13:25:08 +0900

    delivery 추가

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var join = require('./routes/join');
+var delivery = require('./routes/delivery');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/join',join);
+app.use('/delivery', delivery);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
